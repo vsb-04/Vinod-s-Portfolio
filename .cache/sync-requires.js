1d2c6a1c155d057a156e4e6f04cb9052326a6503
Vinod Suresh Badni <139764289+vsb-04@users.noreply.github.com>
2025-04-03 13:28:53 +0530

    Add files via upload

---
+++ .cache/sync-requires.js
@@ -0,0 +1,16 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\pages\\404.js")),
+  "component---src-pages-archive-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\pages\\archive.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\pages\\index.js")),
+  "component---src-pages-pensieve-index-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\pages\\pensieve\\index.js")),
+  "component---src-pages-pensieve-tags-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\pages\\pensieve\\tags.js")),
+  "component---src-templates-post-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\templates\\post.js")),
+  "component---src-templates-tag-js": preferDefault(require("C:\\Users\\VINOD\\Downloads\\v4\\src\\templates\\tag.js"))
+}
+
